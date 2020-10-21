ec716d783bb627d50c35d27c83d8649f027a26e1
Jee <48724771+imjee@users.noreply.github.com>
2020-10-22 02:25:13 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# InsideHeartz-WhatsApp-Sticker-Bot
---
+++ index.js
@@ -0,0 +1,34 @@
+const {
+  create,
+  Client,
+  decryptMedia
+} = require("@open-wa/wa-automate");
+
+async function start(client) {
+  client.onMessage(async (msg) => {
+    // console.log(msg);
+    if (msg.body === "!cek") {
+      client.sendText(msg.from, "👋 Hello! Inside Heartz In Here");
+      client;
+	} else if (msg.body.startsWith("!url ")) {
+	let url = msg.body.split(" ")[1];
+	await client.sendStickerfromUrl(msg.from, url);
+	} else if (msg.body === "!stiker") {
+	if (quotedMsg && quotedMsg.type == 'image') {
+    const mediaData = await decryptMedia(quotedMsg);
+    const imageBase64 = `data:${msg.mimetype};base64,${mediaData.toString(
+          "base64"
+        )}`;
+      await client.sendImageAsSticker(msg.from, imageBase64);
+	  }
+    } else if (msg.mimetype) {
+      if (msg.caption === "!stiker" && msg.type === "image") {
+        const mediaData = await decryptMedia(msg);
+        const imageBase64 = `data:${msg.mimetype};base64,${mediaData.toString(
+          "base64"
+        )}`;
+        await client.sendImageAsSticker(msg.from, imageBase64);
+      }
+    }
+  });
+}
